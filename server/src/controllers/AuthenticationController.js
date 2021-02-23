c35be8ce07e5273489f1e30b76e0f19659ed4f89
Tejas-Nanaware <nanawaret@hotmail.com>
2021-02-23 13:50:23 -0600

    Added jwt authentication

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,5 +1,13 @@
 const { models } = require('../sequelize')
-const { Op } = require('sequelize');
+const { Op } = require('sequelize')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+
+const jwtSignUser = (user) => {
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: config.authentication.jwtExpiresIn
+  })
+}
 
 module.exports = {
   async register (req, res) {
@@ -34,7 +42,11 @@ module.exports = {
           error: 'Credentials not found'
         })
       }
-      res.send(user.toJSON())
+      const userJSON = user.toJSON()
+      res.send({
+        user: userJSON,
+        token: jwtSignUser(userJSON)
+      })
     }).catch(err => {
       res.status(400).send({
         error: 'Error while logging in' + err
